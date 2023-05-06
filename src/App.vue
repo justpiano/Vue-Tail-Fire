c7c6b140b330f041760deefd62968c70403678a0
Diamond <topdeveloper97@gmail.com>
2023-05-06 14:00:00 +0300

    update

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div
+    v-if="isLoading"
+    class="grid place-items-center h-screen"
+  >
+    <Loading class="h-16 w-16" />
+  </div>
+  <div v-else class="flex flex-col min-h-screen">
+    <Navbar />
+    <div class="t-main-set flex-1 grid place-items-center">
+      <router-view v-slot="{ Component, route }">
+        <transition name="slide" mode="out-in">
+          <component :is="Component" :key="route.path" />
+        </transition>
+      </router-view>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import Navbar from "@/components/Navbar.vue";
+import Loading from "@/components/Loading.vue";
+import Footer from "@/components/Footer.vue";
+import { getUserState } from "@/firebase";
+export default defineComponent({
+  name: "App",
+  components: {
+    Navbar,
+    Loading,
+    Footer,
+  },
+  setup(props) {
+    const isLoading = ref(true);
+    getUserState().then(() => {
+      isLoading.value = false;
+    }).catch(() => {
+      isLoading.value = false; // Error check if you have properly configured .env.local file
+    });
+    return {
+      isLoading,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.slide-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-leave-active {
+  transition: all 0.2s ease-out;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateX(-30%);
+  opacity: 0;
+}
+</style>
